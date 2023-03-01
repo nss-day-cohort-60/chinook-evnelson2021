1290a27d3b3c11b98ac5c6f3cb1bc7e2265c544e
Emily Nelson <evnelson2021@gmail.com>
2023-03-01 14:34:56 -0600

    12

---
+++ line_item_track_artist.sql
@@ -0,0 +1,8 @@
+SELECT * , Track.Name as "Track Name", Artist.Name as "Artist Name"
+FROM InvoiceLine
+    JOIN Track
+        ON Track.TrackId = InvoiceLine.TrackId
+    JOIN Album
+        ON Album.AlbumId = Track.AlbumId
+    JOIN Artist
+        ON Artist.ArtistId = Album.ArtistId
