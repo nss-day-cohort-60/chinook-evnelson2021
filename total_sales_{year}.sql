e1d2d39eeebcabb04272ae2bb8b4a2db45c36c59
Emily Nelson <evnelson2021@gmail.com>
2023-03-01 14:43:39 -0600

    9

--- total_sales_{year}.sql
+++ total_sales_{year}.sql
@@ -5,3 +5,7 @@ WHERE InvoiceDate LIKE "%2009%"
 SELECT SUM(Total)
 FROM Invoice
 WHERE InvoiceDate LIKE "%2011%"
+
+SELECT DISTINCT (strftime('%Y', InvoiceDate)) as Year, SUM(Total)
+FROM Invoice
+GROUP BY Year
