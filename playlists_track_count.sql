8b5682d37ced8151b600ec397d0b2483dbe14c48
Emily Nelson <evnelson2021@gmail.com>
2023-03-01 16:08:54 -0600

    15

--- playlists_track_count.sql
+++ playlists_track_count.sql
@@ -1,7 +1,5 @@
 SELECT Playlist.Name, COUNT(PlaylistTrack.TrackId) as "Number of Tracks"
 FROM PlaylistTrack
-    JOIN Playlist
+    LEFT JOIN Playlist
         ON PlaylistTrack.PlaylistId  = Playlist.PlaylistId
-    JOIN Track
-        ON Track.TrackId = PlaylistTrack.TrackId
 GROUP BY PlaylistTrack.PlaylistId
