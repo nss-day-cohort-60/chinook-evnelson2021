72726a5de13b95c0738ae4254d9d64ad577f84e2
Emily Nelson <evnelson2021@gmail.com>
2023-03-02 14:18:28 -0600

    25

--- top_5_tracks.sql
+++ top_5_tracks.sql
@@ -1,1 +1,7 @@
 SELECT Track.Name as Name, COUNT(InvoiceLine.TrackId) as TrackCount
+FROM InvoiceLine
+    JOIN Track 
+        ON InvoiceLine.TrackId = Track.TrackId
+GROUP BY Track.Name
+ORDER BY TrackCount DESC
+LIMIT 5
