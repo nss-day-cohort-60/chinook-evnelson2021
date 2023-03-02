b08852e4e2f1abb5dc68b6133a589d5c1689823b
Emily Nelson <evnelson2021@gmail.com>
2023-03-02 09:37:48 -0600

    18

---
+++ sales_agent_total_sales.sql
@@ -0,0 +1,8 @@
+SELECT Employee.FirstName || ' ' || Employee.LastName AS FullName, ROUND(SUM(Invoice.Total), 2) as TotalSales
+FROM Employee
+    JOIN Invoice
+        ON Invoice.CustomerId = Customer.CustomerId
+    JOIN Customer
+        ON Employee.EmployeeId = Customer.SupportRepId
+WHERE Employee.Title = "Sales Support Agent"
+GROUP BY FullName
